26a77c593df7b46142c2a2b725b906bb7c12f8b5
Abdullah <Abdullahnaseem27@gmail.com>
2025-01-10 23:53:14 +0500

    Add PostgresConnection class for database connection management

---
+++ app/database/connection.py
@@ -0,0 +1,57 @@
+import os
+import logging
+import psycopg2
+from dotenv import load_dotenv
+from psycopg2.extensions import connection as PGConnection
+from typing import Optional
+
+logging.basicConfig(level=logging.INFO)
+
+class PostgresConnection:
+    def __init__(
+        self,
+        user: Optional[str] = None,
+        password: Optional[str] = None,
+        host: Optional[str] = None,
+        port: Optional[str] = None,
+        database: Optional[str] = None
+    ) -> None:
+        load_dotenv()
+        self._user = user or os.getenv("DB_USER")
+        self._password = password or os.getenv("DB_PASSWORD")
+        self._host = host or os.getenv("DB_HOST")
+        self._port = port or os.getenv("DB_PORT")
+        self._database = database or os.getenv("DB_NAME")
+        self._connection: Optional[PGConnection] = None
+
+    def __enter__(self) -> PGConnection:
+        self.connect()
+        return self._connection
+
+    def __exit__(self, exc_type, exc_val, exc_tb) -> None:
+        self.close_connection()
+
+    def connect(self) -> None:
+        if self._connection is None or self._connection.closed:
+            try:
+                self._connection = psycopg2.connect(
+                    user=self._user,
+                    password=self._password,
+                    host=self._host,
+                    port=self._port,
+                    database=self._database,
+                    sslmode="require",
+                )
+                logging.info("Connection established successfully.")
+            except psycopg2.Error as e:
+                logging.error(f"Error connecting to PostgreSQL: {e}")
+                raise
+
+    def close_connection(self) -> None:
+        if self._connection and not self._connection.closed:
+            try:
+                self._connection.close()
+                logging.info("Connection closed successfully.")
+            except psycopg2.Error as e:
+                logging.error(f"Error while closing the connection: {e}")
+                raise
