4e860e5f04b0bcef3d3b520126f75ca6692fa4ca
Abdullah <Abdullahnaseem27@gmail.com>
2025-01-27 23:42:44 +0500

    update constants.py

--- app/constants.py
+++ app/constants.py
@@ -3,4 +3,5 @@ ALLOWED_ORIGINS = [
     "http://localhost:3000/",  
     "https://labmise.com",    # frontend domain
     "https://testbuild.labmise.com/",    # frontend test domain
+    "https://6797d1df7f3d3938af2a7c47--labmise47-v1.netlify.app/",
 ]
